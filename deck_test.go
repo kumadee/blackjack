a95fd93182b7fe1d28e647e190df6b6e020696dc
kumadee <deepak.kumar.ext@gmail.com>
2021-02-27 10:02:12 +0000

    added new tests

--- deck_test.go
+++ deck_test.go
@@ -1,34 +1,37 @@
 package blackjack
 
-import "testing"
+import (
+	"testing"
 
-var deckCases = []struct {
-	description string
-	input       uint
-}{
-	{
-		"Standard deck of size 52.",
-		52,
-	},
-	{
-		"Deck of size 13.",
-		13,
-	},
-	{
-		"Deck of size 0.",
-		0,
-	},
-	{
-		"Deck of size 1.",
-		1,
-	},
-	{
-		"Deck of size 12.",
-		12,
-	},
-}
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewDeck(t *testing.T) {
+	var deckCases = []struct {
+		description string
+		input       uint
+	}{
+		{
+			"Standard deck of size 52.",
+			52,
+		},
+		{
+			"Deck of size 13.",
+			13,
+		},
+		{
+			"Deck of size 0.",
+			0,
+		},
+		{
+			"Deck of size 1.",
+			1,
+		},
+		{
+			"Deck of size 12.",
+			12,
+		},
+	}
 	for _, tc := range deckCases {
 		actual := NewDeck(tc.input)
 		if len(actual) != int(tc.input) {
@@ -52,5 +55,80 @@ func TestNewDeck(t *testing.T) {
 			}
 		}
 		t.Log("Pass:", tc.description)
+	}
+}
+
+func TestResetDiscardPile(t *testing.T) {
+	var cases = []struct {
+		description  string
+		pile         DiscardPile
+		expectedPile DiscardPile
+	}{
+		{
+			description: "Blank Pile.",
+		},
+		{
+			description:  "Empty discard pile.",
+			pile:         DiscardPile{-1, -1, -1},
+			expectedPile: DiscardPile{-1, -1, -1},
+		},
+		{
+			description:  "Discard pile is partially filled.",
+			pile:         DiscardPile{4, 1, 2, -1, -1},
+			expectedPile: DiscardPile{-1, -1, -1, -1, -1},
+		},
+		{
+			description:  "Discard pile is fully filled.",
+			pile:         DiscardPile{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+			expectedPile: DiscardPile{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+		},
+	}
+	for _, tc := range cases {
+		ResetDiscardPile(tc.pile)
+		assert.Equal(t, tc.expectedPile, tc.pile, tc.description)
+	}
+}
+
+func TestCardExistsInDiscardPile(t *testing.T) {
+	var cases = []struct {
+		description      string
+		inputCardIndex   int
+		inputDiscardPile DiscardPile
+		expected         bool
+	}{
+		{
+			description:    "Discard pile empty",
+			inputCardIndex: 1,
+			expected:       false,
+		},
+		{
+			description:      "Card Exists in discard pile",
+			inputCardIndex:   10,
+			inputDiscardPile: DiscardPile{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+			expected:         true,
+		},
+		{
+			description:      "Negative card index does not exist in pile",
+			inputCardIndex:   -100,
+			inputDiscardPile: DiscardPile{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+			expected:         true,
+		},
+		{
+			description:      "Positive card index does not exist",
+			inputCardIndex:   10,
+			inputDiscardPile: DiscardPile{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1},
+			expected:         false,
+		},
+		{
+			description:      "Negative card index exists in pile.",
+			inputCardIndex:   -1,
+			inputDiscardPile: DiscardPile{-1, -1, -1},
+			expected:         true,
+		},
+	}
+	for _, tc := range cases {
+		assert.Equal(t, tc.expected,
+			CardExistsInDiscardPile(tc.inputCardIndex, tc.inputDiscardPile),
+			tc.description)
 	}
 }
